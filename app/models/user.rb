730281571c5a88b188ec3e19ffab39be0e95d9e0
Ry Ellingson <rycellingson@gmail.com>
2020-04-05 21:16:05 +0200

    added login to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_writer :login
+
   validates :email, uniqueness: true
   validates :username, uniqueness: true
 
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
+
+  def login
+    @login || self.username || self.email
+  end
 end
