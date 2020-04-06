b6616eac8db118a09c8d112fd1abfc09131ea9f2
Ry Ellingson <rycellingson@gmail.com>
2020-04-06 19:58:59 +0200

    disabled rubocop

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,6 @@
 class GamesController < ApplicationController
   def show
+    #select the game by id and store it in a variable
     @game = Game.find(params[:id])
     @problems = @game.problems
   end
