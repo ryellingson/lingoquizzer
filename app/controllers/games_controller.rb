2f5cc7c1675e093d85ea6282490524dfa825ae1d
Ry Ellingson <rycellingson@gmail.com>
2020-04-06 20:11:18 +0200

    comments in the seeds

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,8 @@
 class GamesController < ApplicationController
   def show
-    #select the game by id and store it in a variable
+    #select the game by id and store it in a variable creating an instance
     @game = Game.find(params[:id])
+    #create an instance variable assigning problems to the game instance
     @problems = @game.problems
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,27 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'json'
 
+puts "おはよう"
+puts "destroying game data..."
+
 Game.destroy_all
+
+puts "done"
+puts "creating Hiragana Sprint"
 
 hiragana_sprint = Game.create(name: "Hiragana Sprint")
 
+puts "game created"
+puts "parsing hiragana json"
+
 hiragana_data = JSON.parse(File.read(Rails.root + 'db/data/hiragana.json'))
+
+puts "parsed"
+puts "populating game with problems..."
 
 hiragana_data.each do |problem_data|
   Problem.create!(game: hiragana_sprint, question: problem_data["character"], answer: problem_data["romanization"])
 end
 
 puts "job done"
+puts "Hiragana Sprint is ready よしよし"
