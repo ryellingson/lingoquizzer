a245a24c938a5df29cf74c09c8ca0953441f7e81
Ry Ellingson <rycellingson@gmail.com>
2020-04-18 14:41:49 -0500

    LS 6 double - basic game functionality!

--- app/javascript/components/kana_game.js
+++ app/javascript/components/kana_game.js
@@ -1,17 +1,61 @@
 const initGame = () => {
   const playButton = document.querySelector(".play-button");
   const gameInput = document.querySelector(".game-input");
+  const answers = document.querySelectorAll(".answer");
+  const backButton = document.querySelector(".back-button");
+  const nextButton = document.querySelector(".next-button");
+  const correctCountShow = document.querySelector(".correct-count");
+  const timeShow = document.querySelector(".timer");
 
+  let currentAnswer = answers[0];
+  let currentIndex = 0;
+  let correctCountValue = 0;
+  let timeLeft = 5;
+  let interval;
 
   const startGame = () => {
+    interval = setInterval(updateTimer, 1000);
     playButton.classList.add("hidden");
-    gameInput.classList.remove("hidden");
+    gameInput.parentNode.classList.remove("hidden");
+    gameInput.addEventListener('input', handleInputChange);
+    currentAnswer.parentNode.classList.add("active-row");
+  }
 
-    const answers = document.querySelectorAll(".answer")
-    console.log(answers)
+  const updateTimer = () => {
+    timeLeft -= 1;
+    const sec = timeLeft % 60;
+    console.log(timeLeft);
+    timeShow.innerHTML = `${Math.floor(timeLeft / 60)}:${sec < 10 ? "0" + sec : sec}`;
+    if (timeLeft == 0) { endGame() };
+  }
+
+  const handleInputChange = () => {
+    if (gameInput.value === currentAnswer.dataset.answer) {
+      gameInput.value = "";
+      currentAnswer.innerHTML = currentAnswer.dataset.answer;
+      updateCurrentAnswer("next");
+      correctCountValue += 1;
+      correctCountShow.innerHTML = `<p>${correctCountValue}/46</p>`;
+    if (correctCountValue == 46) { endGame() };
+    }
+  }
+
+  const updateCurrentAnswer = (shift) => {
+    currentAnswer.parentNode.classList.remove("active-row");
+    // if shift = "next" add an index else it subtracts an index
+    currentIndex += (shift == "next" ? 1 : -1);
+    currentAnswer = answers[currentIndex];
+    currentAnswer.parentNode.classList.add("active-row");
+  }
+
+  const endGame = () => {
+    document.querySelector(".game-status").classList.remove("hidden");
+    clearInterval(interval);
   }
 
   playButton.addEventListener('click', startGame);
+  nextButton.addEventListener('click', () => updateCurrentAnswer("next"));
+  backButton.addEventListener('click', () => updateCurrentAnswer("back"));
 }
 
 export { initGame }
