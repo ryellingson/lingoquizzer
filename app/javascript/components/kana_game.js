ca1c4649b92c6415f8fcaaf02f4004ccaa8cc4c4
Ry Ellingson <rycellingson@gmail.com>
2020-04-25 21:58:50 -0500

    sign up modal styling

--- app/javascript/components/kana_game.js
+++ app/javascript/components/kana_game.js
@@ -20,7 +20,7 @@ const initGame = () => {
   let currentAnswer = answers[0];
   let currentIndex = 0;
   let correctCountValue = 0;
-  const initialTime = 3;
+  const initialTime = 120;
   let timeLeft = initialTime;
   let interval;
   let score = 0;
