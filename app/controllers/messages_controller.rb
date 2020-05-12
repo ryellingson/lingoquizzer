94f6ab51b3359f98e32447ee28f2e5a0074f4ce6
Ry Ellingson <rycellingson@gmail.com>
2020-05-11 23:47:09 -0500

    got a message to send :)

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!
   def create
+    @message = Message.new(message_params)
+    @message.user = current_user
+    @message.save
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
