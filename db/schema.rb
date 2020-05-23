38ef0f2cade514755679961b4d9eabc792d7739f
Ry Ellingson <rycellingson@gmail.com>
2020-05-23 14:03:51 -0500

    fixed login bug, setup basic forgot password, tailored json jigen, remodeled games index, added category to games

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_14_021613) do
+ActiveRecord::Schema.define(version: 2020_05_23_181254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_05_14_021613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "icon_based", default: false
+    t.string "category"
   end
 
   create_table "messages", force: :cascade do |t|
