3da61dadab3255d4727db60dd782d6887d511f1f
Ry Ellingson <rycellingson@gmail.com>
2020-06-20 16:27:46 -0500

    tried to add online users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :update_last_seen_at, if: -> { user_signed_in? && (current_user.last_seen_at.nil? || current_user.last_seen_at < 5.minutes.ago) }
+
+  def update_last_seen_at
+    logger.info "Update last seen at timestamp for user id #{current_user.id}"
+    current_user.update_attribute(:last_seen_at, Time.current)
+  end
 
   protected
 
