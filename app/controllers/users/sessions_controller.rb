f8062329dbe88257df0ad15810b4a479b2702b52
Ry Ellingson <rycellingson@gmail.com>
2020-07-15 20:35:30 -0500

    fixed guest bug

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -19,7 +19,17 @@ class Users::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User) && resource.banned?
+      sign_out resource
+      flash[:error] = "This account has been suspended for violation of...."
+      root_path
+    else
+      super
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
