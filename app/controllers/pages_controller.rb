d940b3b027747e100a26c9a81c8a1ab6a2331255
Ry Ellingson <rycellingson@gmail.com>
2020-07-22 22:46:58 -0500

    refactoring and cleaning up code

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,8 @@ class PagesController < ApplicationController
   skip_after_action :verify_authorized, only: [ :home, :leaderboards ]
 
   def home
+    home_images_path = Rails.root.join('app', 'assets', 'images', 'home_page_images')
+    @slide_urls = home_images_path.entries.reject(&File.method(:directory?)).sort
   end
 
   def leaderboards
