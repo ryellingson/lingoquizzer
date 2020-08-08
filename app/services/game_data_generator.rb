096e4803fda8ae58fdf72c35214053e39ea70109
Ry Ellingson <rycellingson@gmail.com>
2020-08-07 22:10:08 -0500

    added services

---
+++ app/services/game_data_generator.rb
@@ -0,0 +1,13 @@
+class GameDataGenerator < ApplicationService
+  def initialize(game, target_language)
+    # loads the japanese game file
+    # sets the target langauge
+  end
+
+  def call
+    # extract the English words from the icons name
+    # use an api to get translations of list into target language
+    # build the formatted hash with the question for target language
+    # create a JSON with a list of question icons and an answers
+  end
+end
