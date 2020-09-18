ecdf9a39befc28524ecdcfb16d9ae30e5d2b55d7
Ry Ellingson <rycellingson@gmail.com>
2020-09-18 13:24:33 -0500

    tweaks

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -10,10 +10,10 @@ class ConversationsController < ApplicationController
 
   def set_slideshow_keywords
     slideshow_keywords_data = {
-      ja: "japan,kyushu,hokkaido",
-      en: "england,united%20states,australia,new%20zealand",
-      es: "mexico,spain,latin%20america,costa%20rica",
-      fr: "france,cameroon,quebec"
+      ja: "japan,kyushu,hokkaido,kanji",
+      en: "england,united%20states,australia,new%20zealand,english",
+      es: "mexico,spain,latin%20america,costa%20rica,ecuador,argentina,chile,colombia",
+      fr: "france,cameroon,quebec,paris,avignon,bordeaux,brittany,lyon"
     }
     @slideshow_keywords = slideshow_keywords_data[@language.language_code.to_sym]
     @slide_urls = ["https://source.unsplash.com/900x900/?#{@slideshow_keywords}"]
