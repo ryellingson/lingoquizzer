71bff0def4e51533ec05690a88ba316941374e2c
Ry Ellingson <rycellingson@gmail.com>
2020-10-07 17:43:50 -0500

    added a job for badges

--- app/models/reputation_change_observer.rb
+++ app/models/reputation_change_observer.rb
@@ -6,7 +6,7 @@ class ReputationChangeObserver
     user = User.where(sash_id: changed_data[:sash_id]).first
     badge = Merit::Badge.find(changed_data[:merit_object][:badge_id])
 
-    NotifyBadgeJob.set(wait: 5.seconds).perform_later(user.id, badge.id)
+    NotifyBadgeJob.set(wait: 3.seconds).perform_later(user.id, badge.id)
     # When did it happened:
     # datetime = changed_data[:granted_at]
   end
