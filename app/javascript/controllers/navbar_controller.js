4cf3f1dc3d88a4bc329795506b98f4466820cc9c
Ry Ellingson <rycellingson@gmail.com>
2020-12-20 15:12:05 -0600

    navbar is responsive and the js is running

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,10 +1,15 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "output" ]
+  static targets = [ "toggle", "nav" ]
 
   connect() {
     console.log("Hello from the navbar controller");
+  }
+
+  showMenu() {
+    console.log('inside show menu');
+    this.navTarget.classList.toggle('show');
   }
 }
 
