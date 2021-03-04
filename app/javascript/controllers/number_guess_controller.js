fa190ef3b4f86b5cf91ed2341fdb61bfe04ec19b
Ry Ellingson <rycellingson@gmail.com>
2021-03-04 12:33:13 -0600

    progress on num game

--- app/javascript/controllers/number_guess_controller.js
+++ app/javascript/controllers/number_guess_controller.js
@@ -1,82 +1,76 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = []
+  static targets = ["guessSubmit", "guessField", "guesses", "lastResult", "lowOrHi", "numbers"]
 
   connect() {
     console.log("Welcome to number guess!");
+    this.numbers = JSON.parse(this.numbersTarget.dataset.numbers);
+    console.log(this.numbers);
+    this.randomNumber = Math.floor(Math.random() * 100) + 1;
+    // parse to int
 
-    let randomNumber = Math.floor(Math.random() * 100) + 1;
-
-    const guesses = document.querySelector('.guesses');
-    const lastResult = document.querySelector('.lastResult');
-    const lowOrHi = document.querySelector('.lowOrHi');
-
-    const guessSubmit = document.querySelector('.guessSubmit');
-    const guessField = document.querySelector('.guessField');
-
-    let guessCount = 1;
-    let resetButton;
-    guessField.focus();
+    this.guessCount = 1;
+    this.guessFieldTarget.focus();
   }
 
   checkGuess() {
     console.log('checking guess')
-    let userGuess = Number(guessField.value);
-    if (guessCount === 1) {
-      guesses.textContent = 'Previous guesses: ';
+    let userGuess = Number(this.guessFieldTarget.value);
+    if (this.guessCount === 1) {
+      this.guessesTarget.textContent = 'Previous guesses: ';
     }
-    guesses.textContent += userGuess + ' ';
+    this.guessesTarget.textContent += userGuess + ' ';
 
-    if (userGuess === randomNumber) {
-      lastResult.textContent = 'Congratulations! You got it right!';
-      lastResult.style.backgroundColor = 'green';
-      lowOrHi.textContent = '';
+    if (userGuess === this.randomNumber) {
+      this.lastResultTarget.textContent = 'Congratulations! You got it right!';
+      this.lastResultTarget.style.backgroundColor = 'green';
+      this.lowOrHiTarget.textContent = '';
       setGameOver();
-    } else if (guessCount === 10) {
-      lastResult.textContent = '!!!GAME OVER!!!';
+    } else if (this.guessCount === 10) {
+      this.lastResultTarget.textContent = '!!!GAME OVER!!!';
       setGameOver();
     } else {
-      lastResult.textContent = 'Wrong!';
-      lastResult.style.backgroundColor = 'red';
-      if(userGuess < randomNumber) {
-        lowOrHi.textContent = 'Last guess was too low!';
-      } else if(userGuess > randomNumber) {
-        lowOrHi.textContent = 'Last guess was too high!';
+      this.lastResultTarget.textContent = 'Wrong!';
+      this.lastResultTarget.style.backgroundColor = 'red';
+      if(userGuess < this.randomNumber) {
+        this.lowOrHiTarget.textContent = 'Last guess was too low!';
+      } else if(userGuess > this.randomNumber) {
+        this.lowOrHiTarget.textContent = 'Last guess was too high!';
       }
     }
 
-    guessCount++;
-    guessField.value = '';
-    guessField.focus();
+    this.guessCount++;
+    this.guessFieldTarget.value = '';
+    this.guessFieldTarget.focus();
   }
 
   setGameOver() {
-    guessField.disabled = true;
-    guessSubmit.disabled = true;
-    resetButton = document.createElement('button');
-    resetButton.textContent = 'Start new game';
-    document.body.appendChild(resetButton);
-    resetButton.addEventListener('click', resetGame);
+    this.guessFieldTarget.disabled = true;
+    this.guessSubmitTarget.disabled = true;
+    this.resetButton = document.createElement('button');
+    this.resetButton.textContent = 'Start new game';
+    document.body.appendChild(this.resetButton);
+    this.resetButton.addEventListener('click', resetGame);
   }
 
   resetGame() {
-    guessCount = 1;
+    this.guessCount = 1;
 
     const resetParas = document.querySelectorAll('.resultParas p');
     for (let i = 0 ; i < resetParas.length ; i++) {
       resetParas[i].textContent = '';
     }
 
-    resetButton.parentNode.removeChild(resetButton);
+    this.resetButton.parentNode.removeChild(this.resetButton);
 
-    guessField.disabled = false;
-    guessSubmit.disabled = false;
-    guessField.value = '';
-    guessField.focus();
+    this.guessFieldTarget.disabled = false;
+    this.guessSubmitTarget.disabled = false;
+    this.guessFieldTarget.value = '';
+    this.guessFieldTarget.focus();
 
-    lastResult.style.backgroundColor = 'white';
+    this.lastResultTarget.style.backgroundColor = 'white';
 
-    randomNumber = Math.floor(Math.random() * 100) + 1;
+    this.randomNumber = Math.floor(Math.random() * 100) + 1;
   }
 }
